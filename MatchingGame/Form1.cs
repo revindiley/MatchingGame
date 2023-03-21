cef0b1e1e1f2ad89da7abbc1b72e975072014a8a
Devin Riley <devin.riley144@gmail.com>
2023-03-21 12:53:38 -0500

    added if statement to keep matching icons on screen

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -68,6 +68,13 @@ namespace MatchingGame
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
                 timer1.Start();
             }
         }
